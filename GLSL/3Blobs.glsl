a2e22e2425f700f45b6f7e32abb1125219bdc7df
AdamAchinger <adamachinger123@gmail.com>
2024-10-12 15:23:09 +0200

    Adding Red Glow

--- GLSL/3Blobs.glsl
+++ GLSL/3Blobs.glsl
@@ -6,7 +6,7 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     vec2 mouse = vec2(iMouse.x, iMouse.y * iResolution.y / iResolution.x);
 
     float r = 0.1;
-    float rSpeed = 3.0;
+    float rSpeed = 4.0;
     
     float xPos = (cos(iTime*rSpeed+1.5) * r*2.0) + 0.5;
     float yPos = (sin(iTime*rSpeed+1.5) * r*2.0) +(0.5 * dim);
@@ -37,6 +37,7 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     float dis2 = smoothstep(0.0005,distance(cent1,uv2),0.025);
     float dis3 = smoothstep(0.0005,distance(cent2,uv2),0.025);
     
+
     float addF = 0.0;
     int steps = int(averageDist*100.0);
     
@@ -55,9 +56,11 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     
     
     // PostProcess 
+    float redMask = max(max(max(dis,dis2),dis3),addF);
+    vec3 redColor = vec3(1,0,0) * redMask;
     
-    vec3 color = vec3(pow(add,5.0));
-    
+    vec3 color = vec3(pow(add,5.0)) * 0.25;
+    color += redColor * 0.5;
     
     fragColor = vec4(color, 1.0);
 }
