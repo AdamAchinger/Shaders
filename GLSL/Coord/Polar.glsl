0ac1099d85d576988a4c402e451892fc364aba32
AdamAchinger <adamachinger123@gmail.com>
2024-10-25 00:04:03 +0200

    update i dont get it

--- GLSL/Coord/Polar.glsl
+++ GLSL/Coord/Polar.glsl
@@ -5,21 +5,20 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
     vec2 cent = vec2(0.5,0.5);
     float PI = 3.141592;
 
-    float r = 0.25;
     float ang = iTime;
     float dotSize = 0.02;
 
-    float uvPy = distance(uv,cent)*2.0;
+    float uvPy = distance(uv,cent);
 
     vec2 uvP = vec2(uv.x,uvPy);
-
     vec2 pos = vec2(0.0,0.0);
 
-    float ptMask = step(length(uvP),dotSize);
+    float ptMask = step(uvPy,dotSize);
 
     vec3 uvBG = vec3(uvPy,0,0);
 
     vec3 color = mix(uvBG,vec3(1,1,1),ptMask);
+    
     
 
     fragColor = vec4(color, 1.0);
