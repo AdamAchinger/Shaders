185023c52ea1226ea8267503e3163b3572d1deb3
AdamAchinger <adamachinger123@gmail.com>
2024-10-28 03:27:24 +0100

    update

--- GLSL/Coord/Polar.glsl
+++ GLSL/Coord/Polar.glsl
@@ -3,23 +3,19 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
     // polar coordinate systems 
     vec2 uv = fragCoord / iResolution.xy;
     vec2 cent = vec2(0.5,0.5);
-    float PI = 3.141592;
 
-    float ang = iTime;
-    float dotSize = 0.02;
+    float r = distance(uv,cent)*2.0;
 
-    float uvPy = distance(uv,cent);
 
-    vec2 uvP = vec2(uv.x,uvPy);
-    vec2 pos = vec2(0.0,0.0);
+    float theta = cos(uv.x);
+    vec2 uvP = vec2(theta,r);
 
-    float ptMask = step(uvPy,dotSize);
 
-    vec3 uvBG = vec3(uvPy,0,0);
+    vec2 Pos = vec2(0.0,0.5);
+    float dist = step(distance(Pos,uvP),0.01);
+    //vec3 color = mix(vec3(uvP,0.0),vec3(1,1,1),dist);
 
-    vec3 color = mix(uvBG,vec3(1,1,1),ptMask);
-    
-    
 
+    vec3 color = vec3(theta);
     fragColor = vec4(color, 1.0);
 }   
