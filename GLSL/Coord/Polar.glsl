48efca6007b2a8f6a80fc752dcd7afe41606dedf
AdamAchinger <adamachinger123@gmail.com>
2024-11-16 09:22:50 +0100

    PolarCoords Works adding atan

--- GLSL/Coord/Polar.glsl
+++ GLSL/Coord/Polar.glsl
@@ -2,18 +2,13 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
 {
     // polar coordinate systems 
     vec2 uv = fragCoord / iResolution.xy;
+    float PI = 3.141592;
 
     uv = uv - vec2(0.5,0.5);
-
-    float theta = sin(uv.y);
-    
     float r = distance(uv,vec2(0.0,0.0));
-    vec2 polar = vec2(r,theta);
+    float theta = (atan(uv.y,uv.x)+PI)/(PI*2.0);
 
-    vec2 Pos = vec2(0.1,0.0);
-    
-    float dist = step(distance(polar,Pos),0.0);
+    vec3 color = vec3(r,theta,0.0);
 
-    vec3 color = vec3(dist);
     fragColor = vec4(color, 1.0);
 }   
