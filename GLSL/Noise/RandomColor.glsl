0d1164b9809aa3341eb7eff77bae5f2413ba49b4
AdamAchinger <adamachinger123@gmail.com>
2024-11-23 11:57:05 +0100

    update

--- GLSL/Noise/RandomColor.glsl
+++ GLSL/Noise/RandomColor.glsl
@@ -7,8 +7,6 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     float randY = fract(sin(seed * 912411.15027) * 63758.1937456);
     float randZ = fract(sin(seed * 144242.12441) * 13758.5492721);
 
-
-    
     vec3 color = vec3(randX,randY,randZ); 
     fragColor = vec4(color, 1.0);
 }
