caf4864a9b3b54edc87db63b29b5c8f72f796118
Adam Achinger <72527645+AdamAchinger@users.noreply.github.com>
2024-11-25 12:14:23 +0100

    sun update

--- GLSL/SolarSystem/Sun.glsl
+++ GLSL/SolarSystem/Sun.glsl
@@ -1,4 +1,12 @@
+//// Fcunctions
 
+float fRand(float seed){
+    float rand1 = fract(sin(seed*185.125) *3375.5453 + seed);
+    return rand1;
+}
+
+
+//// Main 
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {    
     vec2 uv = fragCoord / iResolution.xy;
     float dim = iResolution.y / iResolution.x;
@@ -10,22 +18,47 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     float time = iTime; 
 
     //PolarCord
-    uv = uv - vec2(0.5,0.8);
+    uv = uv - vec2(0.5,0.5);
+
+    float centDistR = distance(uv,cent);
 
     //Sun 
-    vec3 sunColor = vec3(0.9961, 0.3882, 0.1882);
-    vec3 sunAuraColor = vec3(0.8706, 0.6706, 0.4706);
-    float sunSize = 0.5;
+    vec3 sunColor = vec3(0.8706, 0.298, 0.1098);
+    vec3 sunAuraColor = vec3(0.6745, 0.4667, 0.2588);
+    float sunSize = 0.25;
     float SunMaskSolid = step(distance(uv,cent),sunSize*0.6);
-    float SunMask = smoothstep(sunSize,0.0,distance(uv,cent));
-    SunMask += SunMaskSolid;
+
+    float SunMaskSmooth = smoothstep(sunSize,0.0,distance(uv,cent));
+
+
     float SpaceAura = smoothstep(1.0,0.0,distance(uv,cent)) * 0.2;
-    vec3 Sun = mix(mix(black,sunColor,SunMask),sunAuraColor, SpaceAura);
+    float SunHalo1 = smoothstep(0.1*sunSize,0.0,distance(distance(uv,cent),sunSize*0.6));
+    float SunHalo2 = smoothstep(0.05*sunSize,0.00,distance(distance(uv,cent),sunSize*0.6))*1.6;
+
+    //Rays 
+    float angle = (atan(uv.x,uv.y)+PI)/(2.0*PI);
+    float distToAng = 0.0;
+    float iter = 340.0 ;
+    for(float i=0.0; i<iter; i++){
+        float a = i/iter;       
+        
+        float rayTime = sin(iTime*0.005*fRand(a));
+        float ray = smoothstep(0.01,0.0,distance(angle,a+rayTime));
+        float rayDistTime = (sin(iTime*fRand(a))+PI)/(2.0/PI);
+        float rayDist = smoothstep(rayDistTime*0.06,0.0,centDistR);
+
+        distToAng += ray * rayDist ;
+    }
+
+    float sunRays = max(min(SunMaskSmooth * distToAng,1.0) - SunMaskSolid,0.0);
+
+    float SunMask1 =  min(sunRays + SunMaskSolid + SunMaskSmooth + SunHalo1 + SunHalo2,3.1);
+    vec3 Sun = mix(mix(black,sunColor,SunMask1),sunAuraColor, SpaceAura);
 
 
 
-    //Post Process
-    vec3 OUT = vec3(Sun)  ;
+    //Post Proces
+    vec3 OUT = vec3(Sun);
 
     vec3 color = OUT;
     fragColor = vec4(color, 1.0);
