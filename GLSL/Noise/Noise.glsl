5a08efbf9e0310ce60a8be6c041a9c979c3d7f7a
AdamAchinger <adamachinger123@gmail.com>
2024-11-26 19:28:40 +0100

    update

--- GLSL/Noise/Noise.glsl
+++ GLSL/Noise/Noise.glsl
@@ -3,14 +3,12 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     float dim = iResolution.y / iResolution.x;
     uv = vec2(uv.x, uv.y * dim);
 
-    vec2 n1 = fract(uv* 4.0);
+    float uvScale = 11.0;
 
-    vec2 n2 = fract((uv* 9.120) + 18.34);
+    vec2 n1 = floor(uv* uvScale)/uvScale;
 
-    vec2 m = mix(n1,n2,0.5);
-    float k = step(distance(n1,n2),0.5);
     
-    vec3 color = vec3(k);
+    vec3 color = vec3(n1,0.0);
     fragColor = vec4(color, 1.0);
 }
 
