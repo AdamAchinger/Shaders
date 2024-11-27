f4fcf4099673046a6f56305723e76c6d632924f0
Adam Achinger <72527645+AdamAchinger@users.noreply.github.com>
2024-11-27 03:27:14 +0100

    update noise

--- GLSL/Noise/Noise.glsl
+++ GLSL/Noise/Noise.glsl
@@ -1,37 +1,46 @@
-vec2 fade(vec2 t) {
-    return t * t * t * (t * (t * 6.0 - 15.0) + 10.0);
+float rand(vec2 n) {
+    return fract(sin(dot(n, vec2(12.9898, 78.233))) * 43758.5453123);
 }
 
-float perlinNoise(vec2 P) {
-    vec4 Pi = floor(P.xyxy);
-    vec4 Pf = fract(P.xyxy);
-    Pi = mod(Pi, 289.0); // aby uniknąć efektów przycinania
-    vec4 ix = Pi.xzxz;
-    vec4 iy = Pi.yyww;
-    vec4 fx = Pf.xzxz;
-    vec4 fy = Pf.yyww;
-
+float noise(vec2 n) {
+    vec2 b = floor(n);
+    vec2 f = smoothstep(vec2(0.0), vec2(1.0), fract(n));
+    
+    // Losowe wartości na wierzchołkach
+    float v1 = rand(b);
+    float v2 = rand(b + vec2(1.0, 0.0));
+    float v3 = rand(b + vec2(0.0, 1.0));
+    float v4 = rand(b + vec2(1.0, 1.0));
+    
     // Interpolacja
-    float a = dot(rand(ix), fx.x);
-    float b = dot(rand(ix + 1.0), fx.y);
-    float c = dot(rand(ix + 1.0), fy.x);
-    float d = dot(rand(ix + 1.0), fy.y);
-
-    return mix(mix(a, b, fade(fx.y)), mix(c, d, fade(fx.y)), fade(fx.x));
+    float i1 = mix(v1, v2, f.x);
+    float i2 = mix(v3, v4, f.x);
+    return mix(i1, i2, f.y);
 }
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {    
     vec2 uv = fragCoord / iResolution.xy;
     float dim = iResolution.y / iResolution.x;
     uv = vec2(uv.x, uv.y * dim);
+    uv -= vec2(0.5,0.5);
 
-    float uvScale = 11.0;
+    vec2 cent = vec2(0.0,0.0);
 
-    vec2 n1 = floor(uv* uvScale)/uvScale;
+    float uvScale = 42.43; 
+    uv *= uvScale;
 
-    
-    vec3 color = vec3(n1,0.0);
-    fragColor = vec4(color, 1.0);
+    vec2 uvDis = uv * distance(uv,cent) * 1.0;
+
+    // grid for vis
+    float gridX = mod(floor(uvDis.x),2.0);
+    float gridY = mod(floor(uvDis.y),2.0);
+    float grid = (gridX + gridY) - ((gridX * gridY)*2.0);
+
+
+    float noi = noise(uv);
+
+    vec3 OUT = vec3(noi);
+    fragColor = vec4(OUT, 1.0);
 }
 
 
